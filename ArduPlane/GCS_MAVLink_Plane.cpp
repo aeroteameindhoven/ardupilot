b8ef043950319f618bc23c474161f431da47e998
Zachary Kohnen <me@dusterthefirst.com>
2025-05-29 18:04:05 +0200

    Disable `MAV_CMD_SET_TECS_EXTERNAL_NAV` support

--- ArduPlane/GCS_MAVLink_Plane.cpp
+++ ArduPlane/GCS_MAVLink_Plane.cpp
@@ -77,7 +77,7 @@ uint8_t GCS_MAVLINK_Plane::base_mode() const
     }
 
     if (!plane.training_manual_pitch || !plane.training_manual_roll) {
-        _base_mode |= MAV_MODE_FLAG_STABILIZE_ENABLED;        
+        _base_mode |= MAV_MODE_FLAG_STABILIZE_ENABLED;
     }
 
     if (plane.control_mode != &plane.mode_manual && plane.control_mode != &plane.mode_initializing) {
@@ -160,7 +160,7 @@ void GCS_MAVLINK_Plane::send_attitude() const
         omega.z);
 }
 
-void GCS_MAVLINK_Plane::send_attitude_target() 
+void GCS_MAVLINK_Plane::send_attitude_target()
 {
 #if HAL_QUADPLANE_ENABLED
     // Check if the attitude target is valid for reporting
@@ -175,7 +175,7 @@ void GCS_MAVLINK_Plane::send_attitude_target()
 
     const float quat_out[4] {quat.q1, quat.q2, quat.q3, quat.q4};
 
-    const uint16_t typemask = 0; 
+    const uint16_t typemask = 0;
 
     mavlink_msg_attitude_target_send(
         chan,
@@ -187,7 +187,7 @@ void GCS_MAVLINK_Plane::send_attitude_target()
         ang_vel.z,              // bodyframe yaw rate (rad/s)
         throttle);              // Collective thrust, normalized to 0 .. 1
 
-#endif // HAL_QUADPLANE_ENABLED 
+#endif // HAL_QUADPLANE_ENABLED
 }
 
 void GCS_MAVLINK_Plane::send_aoa_ssa()
@@ -465,7 +465,7 @@ void GCS_MAVLINK_Plane::send_hygrometer()
     const auto *airspeed = AP::airspeed();
     if (airspeed == nullptr) {
         return;
-    } 
+    }
     const uint32_t now = AP_HAL::millis();
 
     for (uint8_t i=0; i<AIRSPEED_MAX_SENSORS; i++) {
@@ -688,7 +688,7 @@ MAV_RESULT GCS_MAVLINK_Plane::handle_command_int_guided_slew_commands(const mavl
          // no need to process any new packet/s with the
          //  same airspeed any further, if we are already doing it.
         float new_target_airspeed_cm = packet.param2 * 100;
-        if ( is_equal(new_target_airspeed_cm,plane.guided_state.target_airspeed_cm)) { 
+        if ( is_equal(new_target_airspeed_cm,plane.guided_state.target_airspeed_cm)) {
             return MAV_RESULT_ACCEPTED;
         }
         plane.guided_state.target_airspeed_cm = new_target_airspeed_cm;
@@ -727,7 +727,7 @@ MAV_RESULT GCS_MAVLINK_Plane::handle_command_int_guided_slew_commands(const mavl
         plane.guided_state.target_mav_frame = packet.frame;
 
         const int32_t new_target_alt_cm = packet.z * 100;
-        plane.guided_state.target_location.set_alt_cm(new_target_alt_cm, new_target_alt_frame); 
+        plane.guided_state.target_location.set_alt_cm(new_target_alt_cm, new_target_alt_frame);
         plane.guided_state.target_alt_time_ms = AP_HAL::millis();
 
         // param3 contains the desired vertical velocity (not acceleration)
@@ -811,18 +811,18 @@ MAV_RESULT GCS_MAVLINK_Plane::handle_command_int_packet(const mavlink_command_in
         }
         return MAV_RESULT_FAILED;
 
-    case 43008: { // MAV_CMD_SET_TECS_EXTERNAL_NAV
-        // Example usage of param1 (airspeed_error), param2 (altitude_error), param3 (enable)
-        if (packet.param3 > 0.5f) {
-            plane.TECS_controller._external_nav.airspeed_error = packet.param1;
-            plane.TECS_controller._external_nav.altitude_error = packet.param2;
-            plane.TECS_controller._external_nav.enabled = true;
-        } else {
-            plane.TECS_controller._external_nav.enabled = false;
-        }
+    // case 43008: { // MAV_CMD_SET_TECS_EXTERNAL_NAV
+    //     // Example usage of param1 (airspeed_error), param2 (altitude_error), param3 (enable)
+    //     if (packet.param3 > 0.5f) {
+    //         plane.TECS_controller._external_nav.airspeed_error = packet.param1;
+    //         plane.TECS_controller._external_nav.altitude_error = packet.param2;
+    //         plane.TECS_controller._external_nav.enabled = true;
+    //     } else {
+    //         plane.TECS_controller._external_nav.enabled = false;
+    //     }
 
-        return MAV_RESULT_ACCEPTED;
-    }
+    //     return MAV_RESULT_ACCEPTED;
+    // }
 
 #if AP_PLANE_OFFBOARD_GUIDED_SLEW_ENABLED
     // special 'slew-enabled' guided commands here... for speed,alt, and direction commands
@@ -917,7 +917,7 @@ MAV_RESULT GCS_MAVLINK_Plane::handle_command_int_packet(const mavlink_command_in
         plane.handle_external_hagl(packet);
         return MAV_RESULT_ACCEPTED;
 #endif
-        
+
     default:
         return GCS_MAVLINK::handle_command_int_packet(packet, msg);
     }
